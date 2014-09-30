e90f0a1f0841c6251b8cfa20775bcdc874a4b7a6
Administrator <jmiguez@dlatv.net>
2014-09-30 11:22:32 -0300

    script para correr extracts

---
+++ script_para_correr_los_extracts.sql
@@ -0,0 +1,25 @@
+SET SERVEROUTPUT ON;
+CLEAR BUFFER
+SET ECHO OFF
+SET TERMOUT ON
+SET FEED ON
+CLEAR SCREEN
+
+set timing on
+--alter session set time_zone = 'Mexico/General';
+
+  declare
+     cur_salida sys_refcursor;
+  begin
+
+     APP_DWLOAD.PACK_RACDB_TO_CVDWH.PR_CLIENTE_ORIGEN (P_OUT_CUR=>cur_salida );
+     APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_CLIENTE_ORIGEN', P_dir => 'EXTERNAL_TABLES', P_namefile => 'CLIENTE_ORIGEN.txt');
+
+     APP_DWLOAD.PACK_RACDB_TO_CVDWH.PR_MP_USUARIOS_EXT (P_OUT_CUR=>cur_salida );
+     APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_MP_USUARIOS_EXT', P_dir => 'EXTERNAL_TABLES', P_namefile => 'MP_USUARIOS_EXT.txt');
+
+     APP_DWLOAD.PACK_RACDB_TO_CVDWH.PR_MP_USUARIOS_PASE(P_OUT_CUR=>cur_salida );
+     APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_MP_USUARIOS_PASE', P_dir => 'EXTERNAL_TABLES', P_namefile => 'MP_USUARIOS_PASE.txt');
+
+  end;
+  /
