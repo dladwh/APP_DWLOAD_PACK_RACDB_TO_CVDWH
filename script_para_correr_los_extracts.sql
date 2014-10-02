f14f2926240908e9a0600f41e472a047a2e605bd
Administrator <jmiguez@dlatv.net>
2014-10-02 18:33:07 -0300

    20141002

--- script_para_correr_los_extracts.sql
+++ script_para_correr_los_extracts.sql
@@ -23,12 +23,15 @@ set timing on
 
      APP_DWLOAD.PACK_RACDB_TO_CVDWH.PR_MP_USUARIOS_PASE(P_OUT_CUR=>cur_salida );
      APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_MP_USUARIOS_PASE', P_dir => 'EXTERNAL_TABLES', P_namefile => 'MP_USUARIOS_PASE.txt');
-*/
+
      APP_DWLOAD.PACK_RACDB_TO_CVDWH.PR_MP_USUARIOS_PAGO(P_OUT_CUR=>cur_salida );
      APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_MP_USUARIOS_PAGO', P_dir => 'EXTERNAL_TABLES', P_namefile => 'MP_USUARIOS_PAGO.txt');
      
      APP_DWLOAD.pack_racdb_to_cvdwh.pr_usuario_dispositivo_pago(cur_salida);
      APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_USUARIO_DISPOSITIVO_PAGO', P_dir => 'EXTERNAL_TABLES', P_namefile => 'USUARIO_DISPOSITIVO_PAGO.txt');
-    
+*/     
+     APP_DWLOAD.pack_racdb_to_cvdwh.pr_USUARIO_DISPOSITIVO(cur_salida);
+     APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_USUARIO_DISPOSITIVO', P_dir => 'EXTERNAL_TABLES', P_namefile => 'USUARIO_DISPOSITIVO.txt');    
+
   end;
   /
