9c3a4ffe1a89f3bc2716888e9f10e203dff4453b
Administrator <jmiguez@dlatv.net>
2014-10-02 15:23:18 -0300

    20141002

--- 04)_MP_USUARIOS_PAGO_grant_syn.sql
+++ 04)_MP_USUARIOS_PAGO_grant_syn.sql
@@ -3,3 +3,4 @@ grant select on app_esdc81.mp_usuarios_pago to app_dwload;
 create synonym app_dwload.mp_usuarios_pago for app_esdc81.mp_usuarios_pago;
 
 select * from app_esdc81.mp_usuarios_pago;
+/
