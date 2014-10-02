0ae26e8e816328eaeef67f052dc1a2cff354ac35
Administrator <dladwh@users.noreply.github.com>
2014-10-02 15:57:34 -0300

    Update script_para_correr_los_extracts.sql

--- script_para_correr_los_extracts.sql
+++ script_para_correr_los_extracts.sql
@@ -26,6 +26,9 @@ set timing on
 */
      APP_DWLOAD.PACK_RACDB_TO_CVDWH.PR_MP_USUARIOS_PAGO(P_OUT_CUR=>cur_salida );
      APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_MP_USUARIOS_PAGO', P_dir => 'EXTERNAL_TABLES', P_namefile => 'MP_USUARIOS_PAGO.txt');
-
+     
+     APP_DWLOAD.pack_racdb_to_cvdwh.pr_usuario_dispositivo_pago(cur_salida);
+     APP_DWLOAD.PACK_RACDB_TO_CVDWH.REFCURSOR_TO_FILE(P_cur => cur_salida, P_sep => ',', P_tablename => 'EXT_USUARIO_DISPOSITIVO_PAGO', P_dir => 'EXTERNAL_TABLES', P_namefile => 'USUARIO_DISPOSITIVO_PAGO.txt');
+    
   end;
   /
